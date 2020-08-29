78c409b8288a9e7f4934d1a3a6e2f74d279964bf
Chenaniah7 <1391301699@qq.com>
2020-08-29 15:07:02 +0800

    WebUI自动化测试框架代码上传

---
+++ main.py
@@ -0,0 +1,8 @@
+import pytest
+import pytest_html
+from testcases import tests
+
+
+if __name__ == '__main__':
+    # pytest.main(['-v',  'testcases/tests', '--html=./logs/html_report.html'])
+    pytest.main(['-v', ])
